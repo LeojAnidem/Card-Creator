8b94055ed8c2ea4a8fc9390e7f3f1c6d2fc01473
Leoj_A <jeremykaizer@gmail.com>
2022-03-08 11:48:21 -0500

    Nuevo domElement Agregado img

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-import {_, element, deleteChild} from 'https://github.com/LeojAnidem/Library/blob/master/domFunctions.js'; 
+import {_, element, deleteChild} from 'https://cdn.jsdelivr.net/gh/LeojAnidem/Library/domFunctions.js'; 
+
+const addBtn = _('#addProduct');
+
+// Crear objeto card!
+
+class card {
+    constructor (color, title, productDetail, options, price){
+        this.color = color;
+        this.title = title;
+        this.productDetail = productDetail;
+        this.options = options;
+        this.price = price;
+    }
+
+    dibujar() {
+        //Fondo
+        element('body', 'div', 'card');
+            element('.card', 'div', 'imgBx');
+                element('.imgBx', 'img')
+
+            element('.card', 'div', 'details');
+    }
+}
+
+
+addBtn.addEventListener('click', () => {
+    addBtn.textContent = 'hola';
+
+    // Aqui dentro debera ir todo el codigo de admin!
+
+});
 
