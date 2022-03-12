a545d98e8ec266f1277c64ad417e75361dbf16e0
Leoj_A <jeremykaizer@gmail.com>
2022-03-12 02:03:23 -0500

    Version Alfa

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 import {_, createElement, deleteChild} from 'https://cdn.jsdelivr.net/gh/LeojAnidem/Library@fabe9c6/domFunctions.js'
 
-
-
 const addBtn = _('#addProduct');
-
-
+const inputLightDark = _('#darkMode');
 
 // Crear objeto card!
 
@@ -12,12 +9,6 @@ class Img {
     constructor (src, alt){
         this.src = src;
         this.alt = alt;
-    }
-}
-
-class Options {
-    constructor(textOptions){
-        this.textOptions = textOptions; //TextOptions es un array
     }
 }
 
@@ -33,38 +24,37 @@ class Product {
     }
 
     drawProduct(){
+            createElement('.cards', 'div', `card ${this.id}`);
+                createElement(`.${this.id}`, 'div', 'imgBx');
+                    createElement(`.${this.id} .imgBx`, 'img', this.img.src, this.img.alt);
 
-        createElement('body', 'div', `card ${this.id}`);
-            createElement(`.${this.id}`, 'div', 'imgBx');
-                createElement(`.${this.id} .imgBx`, 'img', this.img.src, this.img.alt);
+                createElement(`.${this.id}`, 'div', 'details');
+                    createElement(`.${this.id} .details`, 'div', 'title');
+                        createElement(`.${this.id} .title`, 'h1', '', this.name);
+                        createElement(`.${this.id} .title`, 'p', '', this.category);
 
-            createElement(`.${this.id}`, 'div', 'details');
-                createElement(`.${this.id} .details`, 'div', 'title');
-                    createElement(`.${this.id} .details .title`, 'h1', '', this.name);
-                    createElement(`.${this.id} .details .title`, 'p', '', this.category);
+                    createElement(`.${this.id} .details`, 'div', 'productDetails');
+                        createElement(`.${this.id} .productDetails`, 'h5', '', 'PRODUCT DETAILS');
+                        createElement(`.${this.id} .productDetails`, 'P', '', this.details);
 
-                createElement(`.${this.id} .details`, 'div', 'productDetails');
-                    createElement(`.${this.id} .details .productDetails`, 'h5', '', 'PRODUCT DETAILS');
-                    createElement(`.${this.id} .details .productDetails`, 'P', '', this.details);
+                    createElement(`.${this.id} .details`, 'form', 'cardOptions');
+                        createElement(`.${this.id} .cardOptions`, 'div', 'size');
+                            createElement(`.${this.id} .size`, 'h5', '', 'SIZE');
+                            createElement(`.${this.id} .size`, 'div', 'selector');
 
-                createElement(`.${this.id} .details`, 'form', 'cardOptions');
-                    createElement(`.${this.id} .details .cardOptions`, 'div', 'size');
-                        createElement(`.${this.id} .cardOptions .size`, 'h5', '', 'SIZE');
-                        createElement(`.${this.id} .cardOptions .size`, 'div', 'selector');
-
-                            for(let i = 0; i < this.options.textOptions.length; i++){
-                                createElement(`.${this.id} .size .selector`, 'div', `selectorItem i${i+1}`);
-                                    createElement(`.${this.id} .selector .i${i+1}`, 'input', 'radio', `${this.id}Item${i+1}`, 'option', true);
-                                    createElement(`.${this.id} .selector .i${i+1}`, 'label', this.options.textOptions[i], `${this.id}Item${i+1}`);
-                            }
-                    
-                    createElement(`.${this.id} .details .cardOptions`, 'div', 'addCart');
-                        createElement(`.${this.id} .cardOptions .addCart`, 'div', 'price');
-                            createElement(`.${this.id} .addCart .price`, 'h2', '', '$');
-                            createElement(`.${this.id} .addCart .price`, 'h1', '', this.price);
-                            createElement(`.${this.id} .addCart .price`, 'h3', '', '.99');
+                                for(let i = 0; i < this.options.length; i++){
+                                    createElement(`.${this.id} .selector`, 'div', `selectorItem i${i+1}`);
+                                        createElement(`.${this.id} .i${i+1}`, 'input', 'radio', `${this.id}Item${i+1}`, 'option', true);
+                                        createElement(`.${this.id} .i${i+1}`, 'label', this.options[i], `${this.id}Item${i+1}`);
+                                }
                         
-                        createElement(`.${this.id} .cardOptions .addCart`, 'button', '', 'Add to cart');
+                        createElement(`.${this.id} .cardOptions`, 'div', 'addCart');
+                            createElement(`.${this.id} .addCart`, 'div', 'price');
+                                createElement(`.${this.id} .price`, 'h2', '', '$');
+                                createElement(`.${this.id} .price`, 'h1', '', this.price);
+                                createElement(`.${this.id} .price`, 'h3', '', '.99');
+                            
+                            createElement(`.${this.id} .addCart`, 'button', '', 'Add to cart');
     }
 }
 
@@ -74,34 +64,88 @@ class Card {
         this.product = product;
     }
 
+    applyColor(id){
+
+        let css = `   
+                    .${id} .details{
+                        background: ${this.color};
+                    }
+
+                    .${id} .selectorItem:hover{
+                        color: ${this.color} ;
+                    }
+
+                    .${id} .selectorItem input:checked + label{
+                        color: ${this.color};
+                    }
+
+                    .${id} .addCart button{
+                        color: ${this.color};
+                    }
+
+                    .${id} .addCart button:hover{
+                        background: ${this.color};
+                        color: #ffffff;
+                    }
+                    
+                `;
+
+        let style = document.createElement('style');
+
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+
+        document.getElementsByTagName('head')[0].appendChild(style);
+    }
+
     dibujar() {
         //Fondo
-        this.product.drawProduct();                    
+        this.product.drawProduct();
+                          
     }
 }
 
 let srcImg1 = 'https://www.vans.com.co/arquivos/e-style-banner-4.png';
 let details1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
-let textOptions1 = [36,38,40,42,44];
+let options1 = [36,38,40,42,44];
 
-const options1 = new Options(textOptions1);
 const img1 = new Img(srcImg1, 'Tennis-vans');
 
 let num = 0;
+let id;
 
 addBtn.addEventListener('click', () => {
-    addBtn.textContent = 'hola';
 
     // Aqui dentro debera ir todo el codigo de admin!
 
-    
-    let id = `card${num}`;
+    id = `card${num}`;
     const product1 = new Product(id ,img1, 'Vans MaxQ+', `Men's Shoes`, details1, options1, '159');
 
-    const card1 = new Card('rojo', product1);
+    const card1 = new Card('#016fff', product1);
     card1.dibujar();
-    createElement('body', 'div', 'space');
+    card1.applyColor(id);
+
+    _(`.${id}`).addEventListener('mouseover', function(){
+        _('.cards').scrollLeft = _('.cards').scrollWidth;
+    });
+     
     num++;
+});
+
+inputLightDark.addEventListener('change', function(){
+
+    if(this.checked){
+        document.body.style.background = '#272727';
+        _('.footer').style.color = '#ebebeb';
+    }
+
+    else{
+        document.body.style.background = '#ebebeb';
+        _('.footer').style.color = '#272727';
+    }
 
 });
 
